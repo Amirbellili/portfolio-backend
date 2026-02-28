c257556db9531e648153f33999eb0533b05e2eb3
amir bellili <amirbellili123456@gmail.com>
2026-02-28 17:46:45 +0100

    fix: use port 465 SSL for Gmail

--- dist/config/email.config.js
+++ dist/config/email.config.js
@@ -26,7 +26,7 @@ const createTransporter = async () => {
     return nodemailer_1.default.createTransport({
         host: process.env.EMAIL_HOST,
         port: parseInt(process.env.EMAIL_PORT || '587'),
-        secure: false,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASSWORD
--- src/config/email.config.ts
+++ src/config/email.config.ts
@@ -24,7 +24,7 @@ export const createTransporter = async () => {
   return nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: parseInt(process.env.EMAIL_PORT || '587'),
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASSWORD
